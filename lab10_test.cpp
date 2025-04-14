e185675be5ab4bec4d08c2e333ef3d7b9c8bbc40
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-14 18:50:49 -0500

    altered to n - i when parse past decimal

--- lab10_test.cpp
+++ lab10_test.cpp
@@ -8,6 +8,7 @@ int main(){
     string dbl = "190.8";
     double dub;
     int n = 0;
+    bool afterDecimal = false;
     double tensPower;
     while(dbl[n] != '.'){
         n++;
@@ -16,17 +17,17 @@ int main(){
     cout << dub << endl;
     for(int i = 0; i < dbl.size(); i++){
         if(dbl[i] == '.'){
-            ;
+            afterDecimal = true;
         }else{
             cout << "------------------" << endl;
             cout << dbl[i] << endl;
             cout << n - (i + 1) << endl;
-            tensPower = pow(10, n - (i + 1));
-            dub += ((dbl[i] - '0') * tensPower);
-            //cout << pow(dbl[i],  10 * (n - (i + 1))) << endl;
-            //dub += pow(dbl[i],  10 * (n - (i + 1)));
-            cout << (dbl[i] - '0') * pow(10, n - (i + 1)) << endl;
-            cout << tensPower << endl;
+            if(afterDecimal == true){
+                dub += ((dbl[i] - '0') * pow(10, n - i));
+            }else{
+                dub += ((dbl[i] - '0') * pow(10, n - (i + 1)));
+            }
+            
             cout << dub << endl;
         }
         
