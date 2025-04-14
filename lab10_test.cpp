c0a29250423ca4e088e031a84642dbd0edd032b1
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-14 17:59:33 -0500

    debugging

--- lab10_test.cpp
+++ lab10_test.cpp
@@ -8,15 +8,25 @@ int main(){
     string dbl = "190.8";
     double dub;
     int n = 0;
+    double tensPower;
     while(dbl[n] != '.'){
         n++;
     }
     cout << n << endl;
+    cout << dub << endl;
     for(int i = 0; i < dbl.size(); i++){
         if(dbl[i] == '.'){
             ;
         }else{
-            dub += pow(dbl[i],  n - (i + 1));
+            cout << "------------------" << endl;
+            cout << dbl[i] << endl;
+            cout << n - (i + 1) << endl;
+            tensPower = pow(10, n - (i + 1));
+            dub += dbl[i] * tensPower;
+            //cout << pow(dbl[i],  10 * (n - (i + 1))) << endl;
+            //dub += pow(dbl[i],  10 * (n - (i + 1)));
+            cout << tensPower << endl;
+            cout << dub << endl;
         }
         
     }
