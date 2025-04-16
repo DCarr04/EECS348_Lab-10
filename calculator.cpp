e2045cbe2b319a92d0493b8f741a03773636c87f
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-16 18:36:10 -0500

    copied in parsing/converting string to double code from lab10_test

--- calculator.cpp
+++ calculator.cpp
@@ -36,4 +36,49 @@ double parse_number(const string &expression){
     if (expression[0] == '-'){
         
     }
+
+    //string dbl = ".1908000";
+    double dub;
+    int n = 0;
+    bool afterDecimal = false;
+    bool makeNegative = false;
+    double tensPower;
+    if(dbl[0] == '-'){
+        makeNegative = true;
+        dbl.erase(0, 1);
+    }else if(dbl[0] == '+'){
+        dbl.erase(0, 1);
+    }
+    
+    while(dbl[n] != '.'){
+        n++;
+    }
+    cout << n << endl;
+    cout << dub << endl;
+    for(int i = 0; i < dbl.size(); i++){
+        if (isValid(dbl[i]) != true || (dbl[i] == '.') && afterDecimal == true){
+            cout << "This string is invalid" << endl;
+            return 0;
+        }
+        if(dbl[i] == '.'){
+            afterDecimal = true;
+        }else{
+            cout << "------------------" << endl;
+            cout << dbl[i] << endl;
+            cout << n - (i + 1) << endl;
+            if(afterDecimal == true){
+                dub += ((dbl[i] - '0') * pow(10, n - i));
+            }else{
+                dub += ((dbl[i] - '0') * pow(10, n - (i + 1)));
+            }
+            
+            cout << dub << endl;
+        }
+        
+    }
+
+    if(makeNegative == true){
+        dub *= -1.0;
+    }
+    cout << dub << endl;
 }
