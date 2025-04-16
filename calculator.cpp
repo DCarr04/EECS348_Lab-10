ab2138036f637c64e2ac4dc16c55c1dc5d955c84
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-16 18:34:44 -0500

    copied in isValid() function from lab10_test.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,36 @@
 #include "calculator.hpp"
 #include <string>
+#include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
+bool isValid(char digit){
+    if(digit == '0'){
+        return true;
+    }else if(digit == '1'){
+        return true;
+    }else if(digit == '2'){
+        return true;
+    }else if(digit == '3'){
+        return true;
+    }else if(digit == '4'){
+        return true;
+    }else if(digit == '5'){
+        return true;
+    }else if(digit == '6'){
+        return true;
+    }else if(digit == '7'){
+        return true;
+    }else if(digit == '8'){
+        return true;
+    }else if(digit == '9'){
+        return true;
+    }else if(digit == '.'){
+        return true;
+    }
+
+    return false;
+}
 
 double parse_number(const string &expression){
     if (expression[0] == '-'){
