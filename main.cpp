cea7665bc21f842d67dcf8d02bd53ef44269cadd
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-16 19:04:11 -0500

    added read in file and open file code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #include "calculator.hpp"
+using namespace std;
 
 int main(int argc, char *argv[]) {
+    string file;
+    string line;
+    
+    cout << "Enter a file: ";
+    cin >> file;
+    ifstream myfile (file);
+    if (myfile.is_open()){
+        while(getline(myfile, line)){
+            
+            
+            
+        }
+        
 
+    }
+    myfile.close();
 }
